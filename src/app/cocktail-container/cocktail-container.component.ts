0c7f1ae779ba5f5fb65268cf107c46d54846b9b5
dymafr <erwan1309@hotmail.com>
2022-01-26 18:06:41 +0100

    7 files modified

--- src/app/cocktail-container/cocktail-container.component.ts
+++ src/app/cocktail-container/cocktail-container.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
-import { Observable } from "rxjs";
-import { Cocktail } from "../shared/interfaces/cocktail.interface";
-import { CocktailService } from "../shared/services/cocktail.service";
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cocktail } from '../shared/interfaces/cocktail.interface';
+import { CocktailService } from '../shared/services/cocktail.service';
 
 @Component({
-  selector: "app-cocktail-container",
-  templateUrl: "./cocktail-container.component.html",
-  styleUrls: ["./cocktail-container.component.scss"]
+  selector: 'app-cocktail-container',
+  templateUrl: './cocktail-container.component.html',
+  styleUrls: ['./cocktail-container.component.scss'],
 })
 export class CocktailContainerComponent {
   public cocktails$: Observable<Cocktail[]> = this.cocktailService.cocktails$;
